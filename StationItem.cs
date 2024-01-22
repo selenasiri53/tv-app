cb38876deb1e2bd26b357f1568022dfb8fe2c63a
selenasiri53 <selenasiri53@gmail.com>
2024-01-21 23:01:00 -0500

    card margins

---
+++ StationItem.cs
@@ -0,0 +1,10 @@
+public class StationItem {
+    public int id { get; set; }
+
+    public string CallLetters { get; set; }
+    public string Market { get; set; }
+    public string Affiliate { get; set; }
+
+    public string websiteUrl { get; set; }
+    public string logoUrl { get; set; }
+}
